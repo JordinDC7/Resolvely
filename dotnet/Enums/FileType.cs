aeb5294298671b145588ac547c5e58aee729e7ea
JordinDC7 <141556500+JordinDC7@users.noreply.github.com>
2024-01-19 14:53:01 -0800

    Add files via upload

---
+++ dotnet/Enums/FileType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Enums
+{
+    public enum FileType
+    {
+        jpg = 1,
+        pdf = 2,
+        jpeg = 3,
+        doc = 4,
+        png = 5,
+        gif = 6,    
+        webp = 7,
+        svg = 8,
+        html = 9,
+        xhtml = 10
+    }
+
+}
