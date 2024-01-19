b4777898a276da6070a232450314486692330f66
JordinDC7 <141556500+JordinDC7@users.noreply.github.com>
2024-01-19 14:09:09 -0800

    Add files via upload

---
+++ dotnet/Interfaces/IUserAuthData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Sabio.Models
+{
+    public interface IUserAuthData
+    {
+        int Id { get; }
+        string Email { get; }
+        string Role { get; }
+        object TenantId { get; }
+
+    }
+}
