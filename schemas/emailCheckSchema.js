eb76483de0db7ae03bff551cad95765f32e5849f
JordinDC7 <141556500+JordinDC7@users.noreply.github.com>
2024-01-19 15:48:55 -0800

    Delete schemas directory

--- schemas/emailCheckSchema.js
+++
@@ -1,9 +0,0 @@
-import * as Yup from "yup";
-
-const emailSchema = Yup.object().shape({
-  email: Yup.string()
-    .email("Must use a valid email address.")
-    .required("Email is required"),
-});
-
-export default emailSchema;
